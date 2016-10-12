19e9d96ae0cbef0b5ad120028e04a7b24c93cae9
Brian Rebel <brebel@fnal.gov>
2016-10-12 10:50:28 -0500

    Remove LArTPC wires and planes from gdml file, change LAr to GAr as well.  Add macro to visualize the geometry and look for overlaps.

---
+++ Geometry/gdml/geo.C
@@ -0,0 +1,63 @@
+//
+//  geo.C
+//  garsoft-mrb
+//
+//  Created by Brian Rebel on 10/12/16.
+//  Copyright © 2016 Brian Rebel. All rights reserved.
+//
+
+typedef struct _drawopt {
+  const char* volume;
+  int         color;
+} drawopt;
+
+void geo(TString volName="volCryostat"){
+  
+  gSystem->Load("libGeom");
+  gSystem->Load("libGdml");
+  
+  TGeoManager::Import("generic_detector.gdml");
+  
+  drawopt opt[] = {
+      //   {"volWorld",                 0},
+      //   {"volDetEnclosure",          kWhite},
+      //   {"volCryostat",              kOrange},
+      //   {"volTPCWirePlaneLengthSide", kCyan+3},
+      //   {"volTPCWirePlaneWidthSide", kRed},
+    {"volTPCWidthFace",    kRed},
+    {"volTPCLengthFace",   kCyan+5},
+    {"volTPCBottomFace",   kOrange},
+    {"volTubBottom",       kOrange+7},
+    {"voltheX",            kOrange+7},
+    {"volArgon_solid_L",   kRed},
+    {"volArgon_cap_L",     kOrange},
+    {"volArgon_cap_front", kOrange},
+    {"volTPC",             kOrange},
+    {"volDetEnclosure",    kBlue},
+    {"volTPCActive",       kGreen},
+    {0, 0}
+  };
+  
+  /*for (int i=0;; ++i) {
+   if (opt[i].volume==0) break;
+   gGeoManager->FindVolumeFast(opt[i].volume)->SetLineColor(opt[i].color);
+   }*/
+  
+  TList* mat = gGeoManager->GetListOfMaterials();
+  TIter next(mat);
+  TObject *obj;
+  // while (obj = next()) {
+  //  obj->Print();
+  // }
+
+  gGeoManager->CheckOverlaps(0.01);
+  gGeoManager->PrintOverlaps();
+  gGeoManager->SetMaxVisNodes(70000);
+
+  //gGeoManager->GetTopVolume()->Draw();
+  //gGeoManager->FindVolumeFast(volName)->Draw();
+
+  TFile *tf = new TFile("generic_detector.root", "RECREATE");
+  gGeoManager->Write();
+  tf->Close();
+}
