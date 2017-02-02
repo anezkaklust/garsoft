9274f6567006856aa6c7b6124cd31c22b1069ab9
Brian Rebel <brebel@fnal.gov>
2017-02-02 14:55:25 -0500

    Make the ordering of EnergyDeposits use the absolute value of the track id as we want the secondary particle energy depositions to be sorted with the primary particle depositions.

--- SimulationDataProducts/EnergyDeposit.cxx
+++ SimulationDataProducts/EnergyDeposit.cxx
@@ -26,14 +26,17 @@ namespace gar {
     {}
     
     //-------------------------------------------------
-    // order the energy deposits by time.  If two deposits happen at the same
-    // time, order them by z position in the detector
+    // order the energy deposits by id, then time, then z position
     bool gar::sdp::EnergyDeposit::operator<(gar::sdp::EnergyDeposit const& b) const
     {
-      if( fTrackID < b.TrackID() ) return true;
-      
-      if( fTrackID == b.TrackID() ){
-        if( fTime < b.Time() ) return true;
+      if( std::abs(fTrackID) <  std::abs(b.TrackID()) )
+        return true;
+      else if( std::abs(fTrackID) == std::abs(b.TrackID()) ){
+        if( fTime < b.Time() )
+          return true;
+        else if(fTime == b.Time() ){
+          if(fZ < b.Z()) return true;
+        }
       }
 
       return false;
