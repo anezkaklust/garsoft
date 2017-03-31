d58bffe356498ce4d483a873ff4e5501cd6b7546
Brian Rebel <brebel@fnal.gov>
2017-03-31 13:22:35 -0500

    Get the ReconstructionDataProducts dictionary to build.

--- ReconstructionDataProducts/classes.h
+++ ReconstructionDataProducts/classes.h
@@ -5,6 +5,12 @@
 #ifndef classes_h
 #define classes_h
 
+#include "canvas/Persistency/Common/Wrapper.h"
+#include "canvas/Persistency/Common/Assns.h"
+
+#include "nusimdata/SimulationBase/MCParticle.h"
+
+#include "RawDataProducts/RawDigit.h"
 #include "ReconstructionDataProducts/Hit.h"
 #include "ReconstructionDataProducts/Track.h"
 #include "ReconstructionDataProducts/Shower.h"
