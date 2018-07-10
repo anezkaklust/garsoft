c32fb6b251bfdeaca6f50d5c2a53db935995c933
Thomas Junk <trj@fnal.gov>
2018-07-10 15:27:28 -0500

    debug initial track parameter calculation

--- Reco/tracker1_module.cc
+++ Reco/tracker1_module.cc
@@ -57,7 +57,7 @@ namespace gar {
 
       float fHitResolYZ;           ///< resolution in cm of a hit in YZ (pad size)
       float fHitResolX;            ///< resolution in cm of a hit in X (drift direction)
-      float fSigmaRoad;            ///< how many sigma away from a track a hit can be and still add it
+      float fSigmaRoad;            ///< how many sigma away from a track a hit can be and still add it during patrec
       float fXGapToEndTrack;       ///< how big a gap must be before we end a track and start a new one
       unsigned int fMinNumHits;    ///< minimum number of hits to define a track
       std::string fHitLabel;       ///< label of module creating hits
@@ -65,21 +65,27 @@ namespace gar {
       int fTrackPass;              ///< which pass of the tracking to save as the tracks in the event
       int fDumpTracks;             ///< 0: do not print out tracks, 1: print out tracks
 
+      float fHitResolYZinFit;      ///< Hit resolution parameter to use in fit
+      float fRoadYZinFit;          ///< cut in cm for dropping hits from tracks in fit
+
       int KalmanFit( art::ValidHandle<std::vector<Hit> > &hitHandle, 
 		     std::vector<std::vector<int> > &hitlist, 
 		     std::vector<int> &hsi, 
 		     int itrack, 
-		     bool Forwards, 
+		     bool isForwards, 
 		     std::vector<float> &trackparatend,
 		     float &chisquared,
 		     float &length,
-		     float *covmat); // 5x5 covariance matrix
+		     float *covmat,    // 5x5 covariance matrix
+		     std::vector<int> &unused_hits);
 
-      size_t ifob(size_t ihit, size_t nhits, bool Forwards);
+      size_t ifob(size_t ihit, size_t nhits, bool isForwards);
  
       float capprox(float x1,float y1,
 		    float x2,float y2,
-		    float x3,float y3);  ///< initial guess of curvature calculator
+		    float x3,float y3);  ///< initial guess of curvature calculator -- from ALICE
+
+      float capprox2(float y0, float z0, float y1, float z1, float y2, float z2);  // redo -- returns abs value of curvature
 
     };
 
@@ -90,14 +96,16 @@ namespace gar {
       produces< std::vector<rec::Track> >();
       produces< art::Assns<rec::Hit, rec::Track> >();
 
-      fHitResolYZ     = p.get<float>("HitResolYZ",1.0); // TODO -- think about what this value is
-      fHitResolX      = p.get<float>("HitResolX",0.5);  // this is probably much better
-      fSigmaRoad      = p.get<float>("SigmaRoad",5.0);
-      fMinNumHits     = p.get<unsigned int>("MinNumHits",20);
-      fHitLabel       = p.get<std::string>("HitLabel","hit");
-      fPrintLevel     = p.get<int>("PrintLevel",0);
-      fTrackPass      = p.get<int>("TrackPass",2);
-      fDumpTracks     = p.get<int>("DumpTracks",2);
+      fHitResolYZ      = p.get<float>("HitResolYZ",1.0); // TODO -- think about what this value is
+      fHitResolX       = p.get<float>("HitResolX",0.5);  // this is probably much better
+      fSigmaRoad       = p.get<float>("SigmaRoad",5.0);
+      fMinNumHits      = p.get<unsigned int>("MinNumHits",20);
+      fHitLabel        = p.get<std::string>("HitLabel","hit");
+      fPrintLevel      = p.get<int>("PrintLevel",0);
+      fTrackPass       = p.get<int>("TrackPass",2);
+      fDumpTracks      = p.get<int>("DumpTracks",2);
+      fHitResolYZinFit = p.get<float>("HitResolYZinFit",4.0);
+      fRoadYZinFit     = p.get<float>("RoadYZinFit",1.0);
     }
 
     void tracker1::produce(art::Event & e)
@@ -268,16 +276,19 @@ namespace gar {
 	      std::vector<float> tparend(6);
 	      float chisqforwards = 0;
 	      float lengthforwards = 0;
-	      int retcode = KalmanFit(hitHandle,hitlist,hsi,itrack,true,tparend,chisqforwards,lengthforwards,covmatend);
+	      std::vector<int> unused_hits;  
+	      int retcode = KalmanFit(hitHandle,hitlist,hsi,itrack,true,tparend,chisqforwards,lengthforwards,covmatend,unused_hits);
 	      if (retcode != 0) continue;
+              unused_hits.clear();  // todo -- use these hits for something
 
 	      // the "backwards" fit is in decreasing x.  Track paramters are at the end of the fit, the other end of the track
 
 	      std::vector<float> tparbeg(6);
 	      float chisqbackwards = 0;
 	      float lengthbackwards = 0;
-	      retcode = KalmanFit(hitHandle,hitlist,hsi,itrack,false,tparbeg,chisqbackwards,lengthbackwards,covmatbeg);
+	      retcode = KalmanFit(hitHandle,hitlist,hsi,itrack,false,tparbeg,chisqbackwards,lengthbackwards,covmatbeg,unused_hits);
 	      if (retcode != 0) continue;
+              unused_hits.clear();  // todo -- use these hits for something
 
 	      firstpass_tracks.emplace_back(lengthforwards,
 					    lengthbackwards,
@@ -348,14 +359,17 @@ namespace gar {
 	      std::vector<float> tparend(6);
 	      float chisqforwards = 0;
 	      float lengthforwards = 0;
-	      int retcode = KalmanFit(hitHandle,hitlist2,hsi,itrack,true,tparend,chisqforwards,lengthforwards,covmatend);
+	      std::vector<int> unused_hits;
+	      int retcode = KalmanFit(hitHandle,hitlist2,hsi,itrack,true,tparend,chisqforwards,lengthforwards,covmatend,unused_hits);
 	      if (retcode != 0) continue;
+              unused_hits.clear();  // todo -- use these hits for something
 
 	      std::vector<float> tparbeg(6);
 	      float chisqbackwards = 0;
 	      float lengthbackwards = 0;
-	      retcode = KalmanFit(hitHandle,hitlist2,hsi,itrack,false,tparbeg,chisqbackwards,lengthbackwards,covmatbeg);
+	      retcode = KalmanFit(hitHandle,hitlist2,hsi,itrack,false,tparbeg,chisqbackwards,lengthbackwards,covmatbeg,unused_hits);
 	      if (retcode != 0) continue;
+              unused_hits.clear();  // todo -- use these hits for something
 
 	      secondpass_tracks.emplace_back(lengthforwards,
 					     lengthbackwards,
@@ -439,6 +453,42 @@ namespace gar {
       return c2;
     }
 
+    // redo this using y and z as coordinates, and also return a positive value.  The caller needs to decide the
+    // screw orientation of the helix -- not enough to go on with just y and z coordinates
+
+    float tracker1::capprox2(float y0, float z0, float y1, float z1, float y2, float z2)
+    {
+      float A = y1*(z2 - z0) - z1*(y2 - y0) + (y2*z0 - z2*y0);
+
+      float B = -( (y1*y1 + z1*z1)*(z2 - z0)
+		   -z1*( (y2*y2 + z2*z2) - (y0*y0 + z0*z0) )
+		   + ( (y2*y2 + z2*z2)*z0 - z2*(y0*y0 + z0*z0) ) );
+
+      float C = (y1*y1 + z1*z1)*(y2 - y0)
+	-y1*( (y2*y2 + z2*z2) - (y0*y0 + z0*z0))
+	+ ( (y2*y2 + z2*z2)*y0 - y2*(y0*y0 + z0*z0) );
+
+      float D = - ( (y1*y1 + z1*z1)*(y2*z0 - z2*y0) 
+		    - y1*( (y2*y2 + z2*z2)*z0 - z2*(y0*y0 + z0*z0) ) 
+		    + z1*( (y2*y2 + z2*z2)*y0 - y2*(y0*y0 + z0*z0) ));
+
+      if (TMath::Abs(A) < 1E-10) 
+	{ return 0;}
+
+      float yc = -B/(2.0*A);
+      float zc = -C/(2.0*A);
+      float rs = yc*yc + zc*zc -D/A;
+
+      if (fPrintLevel > 1)
+	{
+	  std::cout << " In capprox2, A, B, C, D, rs: " << A << " " << B << " " << C << " " << D << " " << rs << std::endl;
+	}
+      if (rs <= 0) 
+	{ throw cet::exception("tracker1capprox2: negative input to sqrt"); }
+      float curv = 1.0/TMath::Sqrt(rs);
+      return curv;
+    }
+
     //--------------------------------------------------------------------------------------------------------------
     //--------------------------------------------------------------------------------------------------------------
 
@@ -454,11 +504,12 @@ namespace gar {
 			     std::vector<std::vector<int> >      &hitlist,
 			     std::vector<int>                    &hsi,
 			     int itrack, 
-			     bool Forwards, 
+			     bool isForwards, 
 			     std::vector<float> &trackparatend,
 			     float &chisquared,
 			     float &length,
-			     float *covmat) // 5x5 covariance matrix
+			     float *covmat,                     // 5x5 covariance matrix
+			     std::vector<int> &unused_hits) 
     {
 
       // set some default values in case we return early
@@ -470,11 +521,15 @@ namespace gar {
       for (size_t i=0; i<5; ++i) trackparatend[i] = 0;
       for (size_t i=0; i<25; ++i) covmat[i] = 0;
 
+      float roadsq = fRoadYZinFit*fRoadYZinFit;
+
       // form a rough guess of track parameters
 
-      size_t firsthit = ifob(0,nhits,Forwards);
-      size_t inthit = ifob(fMinNumHits/2,nhits,Forwards);
-      size_t farhit = ifob(fMinNumHits-1,nhits,Forwards);
+      size_t firsthit = ifob(0,nhits,isForwards);
+      size_t inthit = ifob(fMinNumHits/2,nhits,isForwards);
+      size_t farhit = ifob(fMinNumHits-1,nhits,isForwards);
+      //size_t inthit = ifob(nhits/2,nhits,isForwards);
+      //size_t farhit = ifob(nhits-1,nhits,isForwards);
 
       float trackbeg[3] = {hits[hsi[hitlist[itrack][firsthit]]].Position()[0],
 			   hits[hsi[hitlist[itrack][firsthit]]].Position()[1],
@@ -493,7 +548,7 @@ namespace gar {
 	  std::cout << "Printing the first " << fMinNumHits << " hits" << std::endl;
 	  for (size_t i=0;i<fMinNumHits;++i)
 	    {
-	      size_t ihf = ifob(i,nhits,Forwards);
+	      size_t ihf = ifob(i,nhits,isForwards);
 	      std::cout << i << " : " << 
 		hits[hsi[hitlist[itrack][ihf]]].Position()[0] << " " << 
 		hits[hsi[hitlist[itrack][ihf]]].Position()[1] << " " << 
@@ -502,8 +557,8 @@ namespace gar {
 	}
       if (fPrintLevel>0)
 	{
-	  std::cout << "Forwards: " << Forwards << std::endl;
-	  std::cout << "first hit: << " << firsthit << ", inter hit: " << inthit << " " << " far hit: " << farhit << std::endl;
+	  std::cout << "isForwards: " << isForwards << std::endl;
+	  std::cout << "first hit: " << firsthit << ", inter hit: " << inthit << " " << " far hit: " << farhit << std::endl;
 	  std::cout << "in the hit list: " << hsi[hitlist[itrack][firsthit]] << " " << hsi[hitlist[itrack][inthit]] << " " << hsi[hitlist[itrack][farhit]] << std::endl;
 	  std::cout << "First hit x, y, z: " << trackbeg[0] << " " << trackbeg[1] << " " << trackbeg[2] << std::endl;
 	  std::cout << "Inter hit x, y, z: " << tp1[0] << " " << tp1[1] << " " << tp1[2] << std::endl;
@@ -525,12 +580,17 @@ namespace gar {
       else
 	{ return 1; } // got fMinNumHits all at exactly the same value of x (they were sorted).  Reject track.
       // phi in the y,z plane
-      phi_init = TMath::ATan2( tp2[2]-trackbeg[2], tp2[1]-trackbeg[1] ); 
-      curvature_init = capprox(trackbeg[1],trackbeg[2],tp1[1],tp1[2],tp2[1],tp2[2]);
+      phi_init = TMath::ATan2( tp2[1]-trackbeg[1], tp2[2]-trackbeg[2] ); 
+      curvature_init = TMath::Abs(capprox(trackbeg[1],trackbeg[2],tp1[1],tp1[2],tp2[1],tp2[2]));
+      //float curvature_init2 = capprox2(trackbeg[1],trackbeg[2],tp1[1],tp1[2],tp2[1],tp2[2]);
+      //std::cout << "Compare initial curvatures: " << curvature_init << " " <<  curvature_init2 << std::endl;
+
+      if (! isForwards) curvature_init *= -1;
 
       if (fPrintLevel>0)
 	{
-	  std::cout << "initial slope, phi, curvature: " << slope_init << " " << phi_init << " " << curvature_init << std::endl;
+	  std::cout << "phi calc: dz, dy " << tp2[2]-trackbeg[2] << " " <<  tp2[1]-trackbeg[1] << std::endl;
+	  std::cout << "initial curvature, phi, slope: " << curvature_init << " " << phi_init << " " << slope_init << std::endl;
 	}
 
       TMatrixF P(5,5);  // covariance matrix of parameters
@@ -554,8 +614,8 @@ namespace gar {
       // uncertainties on the measured points  (big for now)
       TMatrixF R(2,2);
       R.Zero();
-      R[0][0] = TMath::Sq(fHitResolYZ);  // in cm^2
-      R[1][1] = TMath::Sq(fHitResolYZ);  // in cm^2
+      R[0][0] = TMath::Sq(fHitResolYZinFit);  // in cm^2
+      R[1][1] = TMath::Sq(fHitResolYZinFit);  // in cm^2
 
       // add the hits and update the track parameters and uncertainties.  Put in additional terms to keep uncertainties from shrinking when
       // scattering and energy loss can change the track parameters along the way.
@@ -590,7 +650,7 @@ namespace gar {
       for (size_t ihit=1; ihit<nhits; ++ihit)
 	{
 
-	  size_t ihf = ifob(ihit,nhits,Forwards);
+	  size_t ihf = ifob(ihit,nhits,isForwards);
 	  float xh = hits[hsi[hitlist[itrack][ihf]]].Position()[0];
 	  float yh = hits[hsi[hitlist[itrack][ihf]]].Position()[1];
 	  float zh = hits[hsi[hitlist[itrack][ihf]]].Position()[2];
@@ -670,6 +730,12 @@ namespace gar {
 
 	  ytilde[0] = yh - predstep[0];
 	  ytilde[1] = zh - predstep[1];
+	  float ydistsq = ytilde.Norm2Sqr();
+	  if (ydistsq > roadsq) 
+	    {
+	      unused_hits.push_back(ihf);
+	      continue;
+	    }
 	  chisquared += ytilde.Norm2Sqr()/TMath::Sq(fHitResolYZ); 
 	  if (fPrintLevel > 0)
 	    {
@@ -718,6 +784,22 @@ namespace gar {
 	  trackparatend[i] = parvec[i];
 	}
       trackparatend[5] = xpos;  // tack this on so we can specify where the track endpoint is
+      if (fPrintLevel > 1)
+	{
+	  std::cout << "Track params at end (y, z, curv, phi, slope) " << trackparatend[0] << " " << trackparatend[1] << " " <<
+	    trackparatend[2] << " " << trackparatend[3] <<" " << trackparatend[4] << std::endl;
+	  S.Print();
+	}
+
+      // just for visualization:  put in track parameter guess at beginning as the ending track parameter so we can
+      // use the plotting tools -- remove or comment this out when the initial track parameters are validated
+      //trackparatend[0] = trackbeg[1];
+      //trackparatend[1] = trackbeg[2];
+      //trackparatend[2] = curvature_init;
+      //trackparatend[3] = phi_init;
+      //trackparatend[4] = slope_init;
+      //trackparatend[5] = trackbeg[0];
+
 
       size_t icov=0;
       for (size_t i=0; i<5; ++i)
@@ -735,13 +817,13 @@ namespace gar {
     //--------------------------------------------------------------------------------------------------------------
 
 
-    size_t tracker1::ifob(size_t ihit, size_t nhits, bool Forwards)
+    size_t tracker1::ifob(size_t ihit, size_t nhits, bool isForwards)
     {
       if (ihit >= nhits)
 	{
 	  throw cet::exception("Tracker1") << "Invalid hit index in ifob: " << ihit << " nhits: " << nhits;
 	}
-      if (Forwards)
+      if (isForwards)
 	{
 	  return ihit;
 	}
