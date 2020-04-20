75bd97412afb83b9ef480e1225ea59c908b76e94
ebrianne <eldwan.brianne@desy.de>
2020-04-20 23:53:11 +0200

    Added ClusterCheater module

---
+++ Reco/CaloClusterCheater_module.cc
@@ -0,0 +1,271 @@
+#include <string>
+#include <algorithm>
+
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Principal/Run.h"
+#include "art/Framework/Principal/SubRun.h"
+#include "art/Persistency/Common/PtrMaker.h"
+
+#include "canvas/Utilities/InputTag.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+#include "ReconstructionDataProducts/Track.h"
+#include "ReconstructionDataProducts/CaloHit.h"
+#include "ReconstructionDataProducts/Cluster.h"
+
+#include "Geometry/Geometry.h"
+#include "MCCheater/BackTracker.h"
+#include "DetectorInfo/DetectorClocksService.h"
+#include "DetectorInfo/DetectorPropertiesService.h"
+#include "nutools/EventGeneratorBase/GENIE/EVGBAssociationUtil.h"
+
+#include "RecoAlg/ClusterShapes.h"
+#include "CLHEP/Vector/ThreeVector.h"
+
+namespace gar {
+    namespace rec {
+
+        class eveLoc {
+        public:
+            eveLoc(int id, gar::raw::CellID_t cID)
+            : eveID(id), cellID(cID)
+            {}
+
+            ~eveLoc() {}
+
+            friend bool operator < (eveLoc const& a, eveLoc const& b)
+            {
+                if(a.eveID != b.eveID)
+                return a.eveID < b.eveID;
+
+                return a.cellID < b.cellID;
+            }
+
+            int GetEveID() const { return eveID; }
+
+            gar::raw::CellID_t GetCellID() const { return cellID; }
+
+        private:
+            int                    eveID;
+            gar::raw::CellID_t     cellID;
+        };
+
+        class CaloClusterCheater : public art::EDProducer {
+        public:
+            explicit CaloClusterCheater(fhicl::ParameterSet const& pset);
+
+            // Plugins should not be copied or assigned.
+            CaloClusterCheater(CaloClusterCheater const &) = delete;
+            CaloClusterCheater(CaloClusterCheater &&) = delete;
+            CaloClusterCheater & operator = (CaloClusterCheater const &) = delete;
+            CaloClusterCheater & operator = (CaloClusterCheater &&) = delete;
+
+            // Required functions.
+            void produce(art::Event & e) override;
+
+        private:
+
+            std::string  fMCGeneratorLabel;   ///< label for module to get MC truth information
+            std::string  fCaloHitModuleLabel; ///< label for module creating rec::CaloHit objects
+            std::string  fG4ModuleLabel;      ///< label for module running G4 and making particles, etc
+            unsigned int fMinHits;            ///< minimum number of hits to make a cluster
+
+            gar::geo::GeometryCore const* fGeo;        ///< geometry information
+
+            void CollectHits(const art::Event &evt, const std::string &label, std::vector< art::Ptr<gar::rec::CaloHit> > &hitVector);
+            gar::rec::Cluster CreateCluster(std::vector< art::Ptr<gar::rec::CaloHit> > hitVec);
+        };
+
+        //-----------------------------------------------------------------------------------
+
+        CaloClusterCheater::CaloClusterCheater(fhicl::ParameterSet const& pset) :
+        EDProducer{pset}
+        {
+            fMCGeneratorLabel      = pset.get< std::string  >("MCGeneratorLabel",   "genie"    );
+            fCaloHitModuleLabel    = pset.get< std::string  >("CaloHitModuleLabel", "calohit"  );
+            fG4ModuleLabel         = pset.get< std::string  >("G4ModuleLabel",      "geant"    );
+            fMinHits               = pset.get< unsigned int >("MinHits",            1          );
+
+            fGeo = gar::providerFrom<geo::Geometry>();
+
+            produces< std::vector<gar::rec::Cluster> >();
+            produces< art::Assns<gar::rec::Cluster, gar::rec::CaloHit> >();
+        }
+
+        //-----------------------------------------------------------------------------------
+        void CaloClusterCheater::produce(art::Event & e)
+        {
+            cheat::BackTrackerCore const* bt = gar::providerFrom<cheat::BackTracker>();\
+            // cheat::BackTrackerCore*             bt = const_cast<cheat::BackTrackerCore*>(const_bt);
+
+            //Collect the hits to be passed to the algo
+            std::vector< art::Ptr<gar::rec::CaloHit> > artHits;
+            this->CollectHits(e, fCaloHitModuleLabel, artHits);
+
+            std::map< eveLoc, std::vector< art::Ptr<gar::rec::CaloHit> > > eveCaloHitMap;
+
+            // loop over all hits and fill in the map
+            for( auto const& itr : artHits ) {
+
+                std::vector<gar::cheat::CalIDE> eveides = bt->CaloHitToCalIDEs(itr);
+                // loop over all eveides for this hit
+                for(size_t ieve = 0; ieve < eveides.size(); ieve++) {
+                    // don't worry about eve particles that contribute less than 10% of the
+                    // energy in the current hit
+                    if( eveides[ieve].energyFrac < 0.1) continue;
+
+                    eveLoc el(eveides[ieve].trackID, itr->CellID());
+                    eveCaloHitMap[el].push_back(itr);
+
+                } // end loop over eve IDs for this hit
+            }// end loop over hits
+
+            // loop over the map and make clusters
+            std::unique_ptr< std::vector<gar::rec::Cluster> > clustercol(new std::vector<gar::rec::Cluster>);
+            std::unique_ptr< art::Assns<gar::rec::Cluster, gar::rec::CaloHit> > assn(new art::Assns<gar::rec::Cluster, gar::rec::CaloHit>);
+
+            for(auto hitMapItr : eveCaloHitMap) {
+                if(hitMapItr.second.size() < fMinHits) continue;
+
+                const simb::MCParticle *part = bt->TrackIDToParticle(hitMapItr.first.GetEveID());
+                if(!part) continue;
+
+                gar::rec::Cluster cluster = this->CreateCluster(hitMapItr.second);
+
+                clustercol->emplace_back(cluster);
+                // association the hits to this cluster
+                evgb::util::CreateAssn(*this, e, *clustercol, hitMapItr.second, *assn, clustercol->size() - 1);
+                LOG_INFO("CaloClusterCheater") << "adding cluster: \n" << clustercol->back() << "\nto collection.";
+
+            } // end loop over the map
+
+            e.put(std::move(clustercol));
+            e.put(std::move(assn));
+        }
+
+        //-----------------------------------------------------------------------------------
+        void CaloClusterCheater::CollectHits(const art::Event &evt, const std::string &label, std::vector< art::Ptr<gar::rec::CaloHit> > &hitVector)
+        {
+            art::Handle< std::vector<gar::rec::CaloHit> > theHits;
+            evt.getByLabel(label, theHits);
+
+            if (!theHits.isValid())
+            return;
+
+            for (unsigned int i = 0; i < theHits->size(); ++i)
+            {
+                const art::Ptr<gar::rec::CaloHit> hit(theHits, i);
+                hitVector.push_back(hit);
+            }
+        }
+
+        //-----------------------------------------------------------------------------------
+        gar::rec::Cluster CaloClusterCheater::CreateCluster(std::vector< art::Ptr<gar::rec::CaloHit> > hitVec)
+        {
+            gar::rec::Cluster clu;
+
+            unsigned n = hitVec.size() ;
+            unsigned i = 0 ;
+            float rmin = 9999.;
+            float rmax = 0.;
+            std::vector<float> tmin;
+            std::vector<float> tmax;
+
+            std::vector<float> a, t, x, y, z;
+            a.resize(n);
+            t.resize(n);
+            x.resize(n);
+            y.resize(n);
+            z.resize(n);
+
+            for( auto const &it : hitVec ) {
+                a[i] = it->Energy() ;
+                t[i] = it->Time() ;
+                x[i] = it->Position()[0] ;
+                y[i] = it->Position()[1] ;
+                z[i] = it->Position()[2] ;
+
+                // clu.addHit( it , a[i] ) ;
+
+                //get time of first layer and last layer to compute the difference
+                float r = std::sqrt(y[i]*y[i] + z[i]*z[i]);
+                if( std::fabs(x[i]) < fGeo->GetECALEndcapStartX() ){
+                    //case in barrel
+                    if(rmin >= r)
+                    {
+                        rmin = r;
+                        tmin.push_back(t[i]);
+                    }
+                    if(rmax <= r)
+                    {
+                        rmax = r;
+                        tmax.push_back(t[i]);
+                    }
+                }
+                else{
+                    //case endcap
+                    if( rmin >= std::fabs(x[i]) )
+                    {
+                        rmin = std::fabs(x[i]);
+                        tmin.push_back(t[i]);
+                    }
+                    if( rmax <= std::fabs(x[i]) )
+                    {
+                        rmax = std::fabs(x[i]);
+                        tmax.push_back(t[i]);
+                    }
+                }
+
+                ++i ;
+            }
+
+            //get minimum time of the first layer
+            auto time_first = std::min_element( tmin.begin(), tmin.end() );
+            assert(time_first != tmin.end());
+            //get minimum time of the last layer
+            auto time_last = std::min_element( tmax.begin(), tmax.end() );
+            assert(time_last != tmax.end());
+
+            util::ClusterShapes cs(n, a, t, x, y, z) ;
+
+            clu.setEnergy( cs.getTotalAmplitude()  ) ;
+            clu.setTime( cs.getAverageTime(), *time_first - *time_last ) ;
+            clu.setPosition( cs.getCenterOfGravity() ) ;
+
+            // direction of cluster's PCA
+            float* d = cs.getEigenVecInertia() ;
+            clu.setEigenVectors( d );
+
+            //Main eigenvector
+            CLHEP::Hep3Vector v( d[0], d[1], d[2] ) ;
+
+            clu.setITheta( v.theta() )  ;
+            clu.setIPhi( v.phi() ) ;
+
+            float param[6] ;
+
+            // param[0] = cs.getElipsoid_r1() ;
+            param[0] = cs.getElipsoid_r_forw();
+            param[1] = cs.getElipsoid_r_back();
+            param[2] = cs.getElipsoid_r2() ;
+            param[3] = cs.getElipsoid_r3() ;
+            param[4] = cs.getElipsoid_vol() ;
+            param[5] = cs.getWidth() ;
+
+            clu.setShape( param ) ;
+
+            return clu;
+        }
+
+    } //namespace rec
+} //namespace gar
+
+namespace gar {
+    namespace rec {
+        DEFINE_ART_MODULE(CaloClusterCheater)
+    }
+}
