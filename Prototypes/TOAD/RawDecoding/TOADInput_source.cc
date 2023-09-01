7af344dc48506f77fc645f19a6ed0b90fd4643dd
Naseem Khan <nkhan@dunegpvm02.fnal.gov>
2023-09-01 08:47:35 -0500

    TOAD Channel Mapping and Raw Decoding updated

--- Prototypes/TOAD/RawDecoding/TOADInput_source.cc
+++ Prototypes/TOAD/RawDecoding/TOADInput_source.cc
@@ -3,6 +3,8 @@
 #include "art/Framework/IO/Sources/SourceTraits.h"
 #include "TOADInput.h"
 #include "RawDataProducts/RawDigit.h"
+#include "TOADFrameOverlay.hpp"
+#include "Prototypes/TOAD/ChannelMap/TOADChannelMapService.h"
 
 // maybe make our own timestamp data product if need be.
 // #include "lardataobj/RawData/RDTimeStamp.h"
@@ -17,7 +19,7 @@ gar::TOADInputDetail::TOADInputDetail(
     fHandleSequenceOption(ps.get<std::string>("HandleSequenceOption","ignore")),
     fTrnScale(ps.get<unsigned int>("TrnScale",10000)),
     pmaker(sh) {
-  rh.reconstitutes<std::vector<gar::raw::RawDigit>, art::InEvent>(pretend_module_name);
+    rh.reconstitutes<std::vector<gar::raw::RawDigit>, art::InEvent>(pretend_module_name);
 		   // don't link to lardataobj just to get a RDTimeStamp definition.  If we need one, maybe make a GAr one
 		   // rh.reconstitutes<raw::RDTimeStamp, art::InEvent>(pretend_module_name, "trigger");
 }
@@ -61,11 +63,12 @@ bool gar::TOADInputDetail::readNext(art::RunPrincipal const* const inR,
   if (fUnprocessedEventRecordIDs.empty()) {return false;}
 
   // take the first unprocessed event off the set
-
+  MF_LOG_INFO("HDF5") << "Here 1";
   auto nextEventRecordID_i = fUnprocessedEventRecordIDs.cbegin();
   auto nextEventRecordID = *nextEventRecordID_i;
   fUnprocessedEventRecordIDs.erase(nextEventRecordID_i);
-
+  MF_LOG_INFO("HDF5") << "Here 2";
+ 
   uint32_t run_id = fRawDataFilePtr->get_attribute<uint32_t>("run_number");
 
   // get trigger record header pointer
@@ -73,6 +76,7 @@ bool gar::TOADInputDetail::readNext(art::RunPrincipal const* const inR,
   auto trh = fRawDataFilePtr->get_trh_ptr(nextEventRecordID);
 
   //check that the run number in the trigger record header agrees with that in the file attribute
+  MF_LOG_INFO("HDF5") << "Here 3";
 
   auto run_id2 = trh->get_run_number();
   if (run_id != run_id2)
@@ -89,6 +93,7 @@ bool gar::TOADInputDetail::readNext(art::RunPrincipal const* const inR,
     }
   
   // trigTimeStamp is NOT time but Clock-tick since the epoch.
+  MF_LOG_INFO("HDF5") << "Here 4";
 
   uint64_t trigTimeStamp = trh->get_trigger_timestamp();
   if (fLogLevel > 0)
@@ -104,6 +109,7 @@ bool gar::TOADInputDetail::readNext(art::RunPrincipal const* const inR,
     {
       std::cout << "TOADInput_source: getTrigTime: " << getTrigTime << std::endl;
     }
+  MF_LOG_INFO("HDF5") << "Here 5";
 
   art::Timestamp artTrigStamp (getTrigTime);
   if (fLogLevel > 0)
@@ -126,6 +132,7 @@ bool gar::TOADInputDetail::readNext(art::RunPrincipal const* const inR,
   }
 
   // this is the trigger record ID:
+  MF_LOG_INFO("HDF5") << "Here 6";
 
   int event = nextEventRecordID.first;
   int sequenceno = nextEventRecordID.second;
@@ -164,17 +171,21 @@ bool gar::TOADInputDetail::readNext(art::RunPrincipal const* const inR,
       std::cout << "TOADInput_source: Event Time Stamp :" << outE->time().value() << std::endl;
     }
 
+  MF_LOG_INFO("HDF5") << "Here 7";
 
   std::unique_ptr<std::vector<gar::raw::RawDigit>> rdcol = std::make_unique<std::vector<gar::raw::RawDigit>>();
+  MF_LOG_INFO("HDF5") << "Here 8";
 
   // put code here to unpack the data and put it in rdcol.
   // see the example at duneprototypes/Protodune/hd/RawDecoding/PDHDDataInterfaceWIB3_tool.cc in the method getFragmentsForEvent
   // instead of retrieving the raw file pointer from a service, it's fRawDataFilePtr as a private member of this class
   // use it instead of rf
-
+  
+  art::ServiceHandle<dune::TOADChannelMapService> channelMap;
   dunedaq::hdf5libs::HDF5RawDataFile::record_id_t rid = std::make_pair(event, sequenceno);
   auto sourceids = fRawDataFilePtr->get_source_ids(rid);
-     
+   MF_LOG_INFO("HDF5") << "Here 9";
+    
   for (const auto &source_id : sourceids)  
     {
       // only want detector readout data (i.e. not trigger info)
@@ -194,14 +205,60 @@ bool gar::TOADInputDetail::readNext(art::RunPrincipal const* const inR,
 	  if (fLogLevel > 1)
 	    {
 	      std::cout << "TOADInputSource subdetector string: " << subdetector_string << std::endl;
-	      std::cout << "TOADInputSource looking for subdet: " << " Put string here.  ND_LAr?" << std::endl;
+	      //std::cout << "TOADInputSource looking for subdet: " << " Put string here.  ND_LAr?" << std::endl;
+	      std::cout << "TOADInputSource looking for subdet: " << "ND_GAr" << std::endl;
 	    }
 	  // I stopped copying code here ... Need to check the subdetector string and unpack the data.
+          MF_LOG_INFO("HDF5") << "Here 10";
+
+	  if(subdetector_string == "ND_GAr")
+            {
+              MF_LOG_INFO("HDF5") << "Here 11";
+
+              auto frag = fRawDataFilePtr->get_frag_ptr(rid, source_id);
+              //auto frag_hdr = frag->get_header();
+              //auto frag_ts = frag->get_trigger_timestamp();
+              size_t fhs = sizeof(dunedaq::daqdataformats::FragmentHeader);
+              auto frag_size = frag->get_size() - fhs;
+              MF_LOG_INFO("HDF5") << "frag_size"<<(int)frag_size<<(int)fhs;
+              int frag_start = 0;
+              int frag_interval=0;
+              gar::raw::ADCvector_t adc_vector;
+              MF_LOG_INFO("HDF5") << "Here 12";
+
+              for(frag_start = 0; frag_start<=(int)(frag_size+fhs); frag_start += frag_interval)
+                {
+                auto fdp = static_cast<void*>(static_cast<char*>(frag->get_data()) + frag_interval);
+                auto toad_frame = reinterpret_cast<dunedaq::detdataformats::toad::TOADFrameOverlay*>(static_cast<char*>(fdp));
+                size_t nsamples = toad_frame->n_samples;
+                MF_LOG_INFO("HDF5") << "frag_start"<< frag_start<<"n_samples"<< nsamples;
+                for (size_t i_samp=0; i_samp<nsamples; i_samp++)
+                {
+                  adc_vector.push_back(toad_frame->get_samples(i_samp));
+                  MF_LOG_INFO("HDF5") << "samples"<<(int)(toad_frame->get_samples(i_samp));
+                }
+                auto chaninfo = channelMap->GetChanInfoFromTOADElements (toad_frame->fec);
+                unsigned int offline_chan = chaninfo.offlchan;
+                MF_LOG_INFO("HDF5") << "channel"<< (int)(toad_frame->fec) << "offlchan"<< offline_chan;
+                //unsigned int offline_chan = 0;
+                gar::raw::Compress_t compress = gar::raw::kNone;
+                ULong64_t time = 0;
+                gar::raw::RawDigit rd(offline_chan, adc_vector.size(), adc_vector, compress, time);
+                adc_vector.clear();
+		(*rdcol).push_back(rd);
+                frag_interval = frag_interval + (int)(toad_frame ->n_bytes);
+                MF_LOG_INFO("HDF5") << "Here 13"<< frag_interval;
+
+                }
+            }
 	}
     }
+  MF_LOG_INFO("HDF5") << "Here 14";
 
   put_product_in_principal(std::move(rdcol), *outE, pretend_module_name,
                            "");
+  MF_LOG_INFO("HDF5") << "Here 15";
+
   //put_product_in_principal(std::move(rd_timestamp), *outE, pretend_module_name,
   //                         "trigger");
 
